00631ba733b5679a324dc240dcd7d3ef49eb132c
Kostas Kolomeetz <kolomeetz@gmail.com>
2018-08-09 00:45:29 +0300

    Add eslint configured by airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'airbnb',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  "parser": "babel-eslint",
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'no-unused-vars': [
+      'off'
+    ],
+    'quotes':
+      [
+        'error',
+        'single'
+      ],
+    'semi':
+      [
+        'error',
+        'always'
+      ]
+  }
+};
