dfc8d1de5a03fb1c55df2b93dabadd487f39591c
Kostas Kolomeetz <kolomeetz@gmail.com>
2018-09-29 12:05:54 +0300

    Add email to Profile factory

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :profile do
     user
+    email { Faker::Internet.email }
     name { [user.last_name, user.first_name].join(' ') }
     tagline { Faker::BackToTheFuture.quote }
   end
