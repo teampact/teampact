a11b4acae1ff0adcd08b9d367942055c18e8cd5c
Kostas Kolomeetz <kolomeetz@gmail.com>
2018-10-15 18:17:05 +0300

    Add CSRF token to axios

--- app/javascript/src/api/axios.js
+++ app/javascript/src/api/axios.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 
+axios.defaults.headers.common['X-CSRF-Token'] = document.head.querySelector('[name=csrf-token]').content;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 export default axios;
