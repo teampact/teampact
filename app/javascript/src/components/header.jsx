ba854820442e3845deaf4f7b5c64e4ea955ee842
Kostas Kolomeetz <kolomeetz@gmail.com>
2018-10-23 22:48:53 +0300

    Add navigation to header

--- app/javascript/src/components/header.jsx
+++ app/javascript/src/components/header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const HeaderBlock = styled.div`
   font-size: 1.4rem;
@@ -12,19 +13,42 @@ const HeaderBlock = styled.div`
   background: #eeeeee;
   border-bottom-left-radius: 5px;  
   border-bottom-right-radius: 5px;  
-`
+`;
+
+
+const Navigation = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  
+  li {
+    display: inline-block;
+    margin: 0 5px;
+    padding: 0;
+  }
+`;
 
 class Header extends Component {
   render() {
-    const { current_user } = this.props.currentUser;
+    const { currentUser } = this.props;
+    const { name } = currentUser.current_user;
+
     return (
       <div className="container">
         <HeaderBlock>
           <div>
             teampact
           </div>
+
           <div>
-            {current_user.name}
+            <Navigation>
+              <li><Link to="/">People</Link></li>
+              <li><Link to="/skills">Skills</Link></li>
+            </Navigation>
+          </div>
+
+          <div>
+            {name}
           </div>
         </HeaderBlock>
       </div>
