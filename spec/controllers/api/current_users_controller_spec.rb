28031713287f5823fb37f3adf37cfab0956eec53
Kostas Kolomeetz <kolomeetz@gmail.com>
2018-10-24 11:18:51 +0300

    Do not require rails_helper manually

--- spec/controllers/api/current_users_controller_spec.rb
+++ spec/controllers/api/current_users_controller_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 context 'when user is authorized' do
   describe Api::CurrentUsersController do
     let(:user) { create :user }
--- spec/controllers/api/spaces/profiles_controller_spec.rb
+++ spec/controllers/api/spaces/profiles_controller_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 describe Api::Spaces::ProfilesController do
   let(:space) { create :space }
   let(:profile) { create :profile, space: space }
